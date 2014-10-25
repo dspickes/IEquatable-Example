bc2b84209aea1ebf2d750e76d1233d801ce9425c
dspickes <drew.spickes@gmail.com>
2014-10-25 13:12:58 -0500

    Added WoW character
    Added basic immutable wow character with constructor and specs.

---
+++ Example/WowCharacter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IEquatable.Example
+{
+    public class WowCharacter
+    {
+        public String Name { get; private set; }
+        public String Server { get; private set; }
+
+        public WowCharacter( String name, String server )
+        {
+            Name = name;
+            Server = server;
+        }
+    }
+}
---
+++ Tests/WowCharacterSpecs/When_creating_a_wow_character.cs
@@ -0,0 +1,33 @@
+﻿using IEquatable.Example;
+using NUnit.Framework;
+using System;
+
+namespace IEquatable.Tests.WowCharacterSpecs
+{
+    [TestFixture]
+    public class When_creating_a_wow_character
+    {
+        String givenName = "givenName";
+        String givenServer = "givenServer";
+
+        WowCharacter createdCharacter;
+
+        [TestFixtureSetUp]
+        public void SetupFixture()
+        {
+            createdCharacter = new WowCharacter( givenName, givenServer );
+        }
+
+        [Test]
+        public void The_created_character_name_should_be_the_given_name()
+        {
+            Assert.That( createdCharacter.Name, Is.EqualTo( givenName ) );
+        }
+
+        [Test]
+        public void The_created_character_server_should_be_the_given_server()
+        {
+            Assert.That( createdCharacter.Server, Is.EqualTo( givenServer ) );
+        }
+    }
+}
